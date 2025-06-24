d9a7967b0d1fb237c94b55d8e1a4d7b24e282109
Marv140 <50950885+Marv140@users.noreply.github.com>
2025-06-24 23:31:05 +0200

    fix img url

--- js/mhd_images.js
+++ js/mhd_images.js
@@ -2,414 +2,414 @@
 export default [
   {
     "city": "Brno",
-    "image": "assets/images/mhd_guesser/brno/238398.webp"
+    "image": "/assets/images/mhd_guesser/brno/238398.webp"
   },
   {
     "city": "Brno",
-    "image": "assets/images/mhd_guesser/brno/319988.webp"
+    "image": "/assets/images/mhd_guesser/brno/319988.webp"
   },
   {
     "city": "Brno",
-    "image": "assets/images/mhd_guesser/brno/381189.webp"
+    "image": "/assets/images/mhd_guesser/brno/381189.webp"
   },
   {
     "city": "Brno",
-    "image": "assets/images/mhd_guesser/brno/411288.webp"
+    "image": "/assets/images/mhd_guesser/brno/411288.webp"
   },
   {
     "city": "Brno",
-    "image": "assets/images/mhd_guesser/brno/411965.webp"
+    "image": "/assets/images/mhd_guesser/brno/411965.webp"
   },
   {
     "city": "České Budějovice",
-    "image": "assets/images/mhd_guesser/ceske_budejovice/410111.webp"
+    "image": "/assets/images/mhd_guesser/ceske_budejovice/410111.webp"
   },
   {
     "city": "České Budějovice",
-    "image": "assets/images/mhd_guesser/ceske_budejovice/410189.webp"
+    "image": "/assets/images/mhd_guesser/ceske_budejovice/410189.webp"
   },
   {
     "city": "České Budějovice",
-    "image": "assets/images/mhd_guesser/ceske_budejovice/415427.webp"
+    "image": "/assets/images/mhd_guesser/ceske_budejovice/415427.webp"
   },
   {
     "city": "České Budějovice",
-    "image": "assets/images/mhd_guesser/ceske_budejovice/415430.webp"
+    "image": "/assets/images/mhd_guesser/ceske_budejovice/415430.webp"
   },
   {
     "city": "České Budějovice",
-    "image": "assets/images/mhd_guesser/ceske_budejovice/415987.webp"
+    "image": "/assets/images/mhd_guesser/ceske_budejovice/415987.webp"
   },
   {
     "city": "Chomutov",
-    "image": "assets/images/mhd_guesser/chomutov/395487.webp"
+    "image": "/assets/images/mhd_guesser/chomutov/395487.webp"
   },
   {
     "city": "Chomutov",
-    "image": "assets/images/mhd_guesser/chomutov/395488.webp"
+    "image": "/assets/images/mhd_guesser/chomutov/395488.webp"
   },
   {
     "city": "Chomutov",
-    "image": "assets/images/mhd_guesser/chomutov/397043.webp"
+    "image": "/assets/images/mhd_guesser/chomutov/397043.webp"
   },
   {
     "city": "Chomutov",
-    "image": "assets/images/mhd_guesser/chomutov/403610.webp"
+    "image": "/assets/images/mhd_guesser/chomutov/403610.webp"
   },
   {
     "city": "Chomutov",
-    "image": "assets/images/mhd_guesser/chomutov/411816.webp"
+    "image": "/assets/images/mhd_guesser/chomutov/411816.webp"
   },
   {
     "city": "Děčín",
-    "image": "assets/images/mhd_guesser/decin/254409.webp"
+    "image": "/assets/images/mhd_guesser/decin/254409.webp"
   },
   {
     "city": "Děčín",
-    "image": "assets/images/mhd_guesser/decin/385787.webp"
+    "image": "/assets/images/mhd_guesser/decin/385787.webp"
   },
   {
     "city": "Děčín",
-    "image": "assets/images/mhd_guesser/decin/410620.webp"
+    "image": "/assets/images/mhd_guesser/decin/410620.webp"
   },
   {
     "city": "Děčín",
-    "image": "assets/images/mhd_guesser/decin/411654.webp"
+    "image": "/assets/images/mhd_guesser/decin/411654.webp"
   },
   {
     "city": "Děčín",
-    "image": "assets/images/mhd_guesser/decin/411988.webp"
+    "image": "/assets/images/mhd_guesser/decin/411988.webp"
   },
   {
     "city": "Hradec Králové",
-    "image": "assets/images/mhd_guesser/hradec_kralove/399496.webp"
+    "image": "/assets/images/mhd_guesser/hradec_kralove/399496.webp"
   },
   {
     "city": "Hradec Králové",
-    "image": "assets/images/mhd_guesser/hradec_kralove/415456.webp"
+    "image": "/assets/images/mhd_guesser/hradec_kralove/415456.webp"
   },
   {
     "city": "Hradec Králové",
-    "image": "assets/images/mhd_guesser/hradec_kralove/415569.webp"
+    "image": "/assets/images/mhd_guesser/hradec_kralove/415569.webp"
   },
   {
     "city": "Hradec Králové",
-    "image": "assets/images/mhd_guesser/hradec_kralove/415635.webp"
+    "image": "/assets/images/mhd_guesser/hradec_kralove/415635.webp"
   },
   {
     "city": "Hradec Králové",
-    "image": "assets/images/mhd_guesser/hradec_kralove/416154.webp"
+    "image": "/assets/images/mhd_guesser/hradec_kralove/416154.webp"
   },
   {
     "city": "Jihlava",
-    "image": "assets/images/mhd_guesser/jihlava/354344.webp"
+    "image": "/assets/images/mhd_guesser/jihlava/354344.webp"
   },
   {
     "city": "Jihlava",
-    "image": "assets/images/mhd_guesser/jihlava/403917.webp"
+    "image": "/assets/images/mhd_guesser/jihlava/403917.webp"
   },
   {
     "city": "Jihlava",
-    "image": "assets/images/mhd_guesser/jihlava/412691.webp"
+    "image": "/assets/images/mhd_guesser/jihlava/412691.webp"
   },
   {
     "city": "Jihlava",
-    "image": "assets/images/mhd_guesser/jihlava/413772.webp"
+    "image": "/assets/images/mhd_guesser/jihlava/413772.webp"
   },
   {
     "city": "Jihlava",
-    "image": "assets/images/mhd_guesser/jihlava/415758.webp"
+    "image": "/assets/images/mhd_guesser/jihlava/415758.webp"
   },
   {
     "city": "Karlovy Vary",
-    "image": "assets/images/mhd_guesser/karlovy_vary/412965.jpg"
+    "image": "/assets/images/mhd_guesser/karlovy_vary/412965.jpg"
   },
   {
     "city": "Karlovy Vary",
-    "image": "assets/images/mhd_guesser/karlovy_vary/413051.jpg"
+    "image": "/assets/images/mhd_guesser/karlovy_vary/413051.jpg"
   },
   {
     "city": "Karlovy Vary",
-    "image": "assets/images/mhd_guesser/karlovy_vary/413673.jpg"
+    "image": "/assets/images/mhd_guesser/karlovy_vary/413673.jpg"
   },
   {
     "city": "Karlovy Vary",
-    "image": "assets/images/mhd_guesser/karlovy_vary/414172.jpg"
+    "image": "/assets/images/mhd_guesser/karlovy_vary/414172.jpg"
   },
   {
     "city": "Karlovy Vary",
-    "image": "assets/images/mhd_guesser/karlovy_vary/415202.jpg"
+    "image": "/assets/images/mhd_guesser/karlovy_vary/415202.jpg"
   },
   {
     "city": "Liberec",
-    "image": "assets/images/mhd_guesser/liberec/367925.jpg"
+    "image": "/assets/images/mhd_guesser/liberec/367925.jpg"
   },
   {
     "city": "Liberec",
-    "image": "assets/images/mhd_guesser/liberec/414618.jpg"
+    "image": "/assets/images/mhd_guesser/liberec/414618.jpg"
   },
   {
     "city": "Liberec",
-    "image": "assets/images/mhd_guesser/liberec/414621.jpg"
+    "image": "/assets/images/mhd_guesser/liberec/414621.jpg"
   },
   {
     "city": "Liberec",
-    "image": "assets/images/mhd_guesser/liberec/414626.jpg"
+    "image": "/assets/images/mhd_guesser/liberec/414626.jpg"
   },
   {
     "city": "Liberec",
-    "image": "assets/images/mhd_guesser/liberec/416205.jpg"
+    "image": "/assets/images/mhd_guesser/liberec/416205.jpg"
   },
   {
     "city": "Mladá Boleslav",
-    "image": "assets/images/mhd_guesser/mlada_boleslav/388523.jpg"
+    "image": "/assets/images/mhd_guesser/mlada_boleslav/388523.jpg"
   },
   {
     "city": "Mladá Boleslav",
-    "image": "assets/images/mhd_guesser/mlada_boleslav/396883.jpg"
+    "image": "/assets/images/mhd_guesser/mlada_boleslav/396883.jpg"
   },
   {
     "city": "Mladá Boleslav",
-    "image": "assets/images/mhd_guesser/mlada_boleslav/396890.jpg"
+    "image": "/assets/images/mhd_guesser/mlada_boleslav/396890.jpg"
   },
   {
     "city": "Mladá Boleslav",
-    "image": "assets/images/mhd_guesser/mlada_boleslav/403078.jpg"
+    "image": "/assets/images/mhd_guesser/mlada_boleslav/403078.jpg"
   },
   {
     "city": "Mladá Boleslav",
-    "image": "assets/images/mhd_guesser/mlada_boleslav/403858.jpg"
+    "image": "/assets/images/mhd_guesser/mlada_boleslav/403858.jpg"
   },
   {
     "city": "Mariánské Lázně",
-    "image": "assets/images/mhd_guesser/marianske_lazne/400022.jpg"
+    "image": "/assets/images/mhd_guesser/marianske_lazne/400022.jpg"
   },
   {
     "city": "Mariánské Lázně",
-    "image": "assets/images/mhd_guesser/marianske_lazne/400747.jpg"
+    "image": "/assets/images/mhd_guesser/marianske_lazne/400747.jpg"
   },
   {
     "city": "Mariánské Lázně",
-    "image": "assets/images/mhd_guesser/marianske_lazne/402211.jpg"
+    "image": "/assets/images/mhd_guesser/marianske_lazne/402211.jpg"
   },
   {
     "city": "Mariánské Lázně",
-    "image": "assets/images/mhd_guesser/marianske_lazne/406150.jpg"
+    "image": "/assets/images/mhd_guesser/marianske_lazne/406150.jpg"
   },
   {
     "city": "Mariánské Lázně",
-    "image": "assets/images/mhd_guesser/marianske_lazne/412606.jpg"
+    "image": "/assets/images/mhd_guesser/marianske_lazne/412606.jpg"
   },
   {
     "city": "Most",
-    "image": "assets/images/mhd_guesser/most/404113.jpg"
+    "image": "/assets/images/mhd_guesser/most/404113.jpg"
   },
   {
     "city": "Most",
-    "image": "assets/images/mhd_guesser/most/410567.jpg"
+    "image": "/assets/images/mhd_guesser/most/410567.jpg"
   },
   {
     "city": "Most",
-    "image": "assets/images/mhd_guesser/most/410866.jpg"
+    "image": "/assets/images/mhd_guesser/most/410866.jpg"
   },
   {
     "city": "Most",
-    "image": "assets/images/mhd_guesser/most/415442.jpg"
+    "image": "/assets/images/mhd_guesser/most/415442.jpg"
   },
   {
     "city": "Most",
-    "image": "assets/images/mhd_guesser/most/416258.jpg"
+    "image": "/assets/images/mhd_guesser/most/416258.jpg"
   },
   {
     "city": "Most",
-    "image": "assets/images/mhd_guesser/most/416632.jpg"
+    "image": "/assets/images/mhd_guesser/most/416632.jpg"
   },
   {
     "city": "Olomouc",
-    "image": "assets/images/mhd_guesser/olomouc/394976.jpg"
+    "image": "/assets/images/mhd_guesser/olomouc/394976.jpg"
   },
   {
     "city": "Olomouc",
-    "image": "assets/images/mhd_guesser/olomouc/397745.jpg"
+    "image": "/assets/images/mhd_guesser/olomouc/397745.jpg"
   },
   {
     "city": "Olomouc",
-    "image": "assets/images/mhd_guesser/olomouc/414079.jpg"
+    "image": "/assets/images/mhd_guesser/olomouc/414079.jpg"
   },
   {
     "city": "Olomouc",
-    "image": "assets/images/mhd_guesser/olomouc/414082.jpg"
+    "image": "/assets/images/mhd_guesser/olomouc/414082.jpg"
   },
   {
     "city": "Olomouc",
-    "image": "assets/images/mhd_guesser/olomouc/415795.jpg"
+    "image": "/assets/images/mhd_guesser/olomouc/415795.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/315294.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/315294.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/328143.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/328143.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/344344.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/344344.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/347880.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/347880.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/348396.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/348396.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/376823.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/376823.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/394867.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/394867.jpg"
   },
   {
     "city": "Ostrava",
-    "image": "assets/images/mhd_guesser/ostrava/414961.jpg"
+    "image": "/assets/images/mhd_guesser/ostrava/414961.jpg"
   },
   {
     "city": "Pardubice",
-    "image": "assets/images/mhd_guesser/pardubice/370958.jpg"
+    "image": "/assets/images/mhd_guesser/pardubice/370958.jpg"
   },
   {
     "city": "Pardubice",
-    "image": "assets/images/mhd_guesser/pardubice/412041.jpg"
+    "image": "/assets/images/mhd_guesser/pardubice/412041.jpg"
   },
   {
     "city": "Pardubice",
-    "image": "assets/images/mhd_guesser/pardubice/416081.jpg"
+    "image": "/assets/images/mhd_guesser/pardubice/416081.jpg"
   },
   {
     "city": "Pardubice",
-    "image": "assets/images/mhd_guesser/pardubice/416195.jpg"
+    "image": "/assets/images/mhd_guesser/pardubice/416195.jpg"
   },
   {
     "city": "Pardubice",
-    "image": "assets/images/mhd_guesser/pardubice/416286.jpg"
+    "image": "/assets/images/mhd_guesser/pardubice/416286.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/257581.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/257581.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/409656.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/409656.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/411927.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/411927.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/415213.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/415213.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/415382.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/415382.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/415812.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/415812.jpg"
   },
   {
     "city": "Plzeň",
-    "image": "assets/images/mhd_guesser/plzen/416138.jpg"
+    "image": "/assets/images/mhd_guesser/plzen/416138.jpg"
   },
   {
     "city": "Praha",
-    "image": "assets/images/mhd_guesser/praha/415978.jpg"
+    "image": "/assets/images/mhd_guesser/praha/415978.jpg"
   },
   {
     "city": "Praha",
-    "image": "assets/images/mhd_guesser/praha/416245.jpg"
+    "image": "/assets/images/mhd_guesser/praha/416245.jpg"
   },
   {
     "city": "Praha",
-    "image": "assets/images/mhd_guesser/praha/416287.jpg"
+    "image": "/assets/images/mhd_guesser/praha/416287.jpg"
   },
   {
     "city": "Praha",
-    "image": "assets/images/mhd_guesser/praha/416339.jpg"
+    "image": "/assets/images/mhd_guesser/praha/416339.jpg"
   },
   {
     "city": "Praha",
-    "image": "assets/images/mhd_guesser/praha/416367.jpg"
+    "image": "/assets/images/mhd_guesser/praha/416367.jpg"
   },
   {
     "city": "Teplice",
-    "image": "assets/images/mhd_guesser/teplice/395412.jpg"
+    "image": "/assets/images/mhd_guesser/teplice/395412.jpg"
   },
   {
     "city": "Teplice",
-    "image": "assets/images/mhd_guesser/teplice/411393.jpg"
+    "image": "/assets/images/mhd_guesser/teplice/411393.jpg"
   },
   {
     "city": "Teplice",
-    "image": "assets/images/mhd_guesser/teplice/413029.jpg"
+    "image": "/assets/images/mhd_guesser/teplice/413029.jpg"
   },
   {
     "city": "Teplice",
-    "image": "assets/images/mhd_guesser/teplice/413635.jpg"
+    "image": "/assets/images/mhd_guesser/teplice/413635.jpg"
   },
   {
     "city": "Teplice",
-    "image": "assets/images/mhd_guesser/teplice/413886.jpg"
+    "image": "/assets/images/mhd_guesser/teplice/413886.jpg"
   },
   {
     "city": "Ústí nad Labem",
-    "image": "assets/images/mhd_guesser/usti_nad_labem/407959.jpg"
+    "image": "/assets/images/mhd_guesser/usti_nad_labem/407959.jpg"
   },
   {
     "city": "Ústí nad Labem",
-    "image": "assets/images/mhd_guesser/usti_nad_labem/412959.jpg"
+    "image": "/assets/images/mhd_guesser/usti_nad_labem/412959.jpg"
   },
   {
     "city": "Ústí nad Labem",
-    "image": "assets/images/mhd_guesser/usti_nad_labem/413281.jpg"
+    "image": "/assets/images/mhd_guesser/usti_nad_labem/413281.jpg"
   },
   {
     "city": "Ústí nad Labem",
-    "image": "assets/images/mhd_guesser/usti_nad_labem/413727.jpg"
+    "image": "/assets/images/mhd_guesser/usti_nad_labem/413727.jpg"
   },
   {
     "city": "Ústí nad Labem",
-    "image": "assets/images/mhd_guesser/usti_nad_labem/414943.jpg"
+    "image": "/assets/images/mhd_guesser/usti_nad_labem/414943.jpg"
   },
   {
     "city": "Ústí nad Labem",
-    "image": "assets/images/mhd_guesser/usti_nad_labem/415895.jpg"
+    "image": "/assets/images/mhd_guesser/usti_nad_labem/415895.jpg"
   },
   {
     "city": "Zlín",
-    "image": "assets/images/mhd_guesser/zlin/262553.jpg"
+    "image": "/assets/images/mhd_guesser/zlin/262553.jpg"
   },
   {
     "city": "Zlín",
-    "image": "assets/images/mhd_guesser/zlin/319579.jpg"
+    "image": "/assets/images/mhd_guesser/zlin/319579.jpg"
   },
   {
     "city": "Zlín",
-    "image": "assets/images/mhd_guesser/zlin/393762.jpg"
+    "image": "/assets/images/mhd_guesser/zlin/393762.jpg"
   },
   {
     "city": "Zlín",
-    "image": "assets/images/mhd_guesser/zlin/406595.jpg"
+    "image": "/assets/images/mhd_guesser/zlin/406595.jpg"
   },
   {
     "city": "Zlín",
-    "image": "assets/images/mhd_guesser/zlin/412523.jpg"
+    "image": "/assets/images/mhd_guesser/zlin/412523.jpg"
   },
   {
     "city": "Zlín",
-    "image": "assets/images/mhd_guesser/zlin/416252.jpg"
+    "image": "/assets/images/mhd_guesser/zlin/416252.jpg"
   }
 ];
